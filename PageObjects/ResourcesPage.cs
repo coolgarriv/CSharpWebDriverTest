1e6dd2a385889eb17ce52feee3dfd714f561440c
David Meehan <David.Meehan@aspengrovesolutions.com>
2015-04-15 16:56:39 +0100

    New branch, updated PageObjects files.

--- PageObjects/ResourcesPage.cs
+++ PageObjects/ResourcesPage.cs
@@ -1,10 +1,27 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
 using System;
 
 namespace WebDriverTest
 {
   public class ResourcesPage
   {
-    private IWEDriver driver;
+    private IWEDriver _driver;
+	
+	[FindsBy(How = How.ClassName, Using = "entry-title")]
+	private IWebElement h1PageTitle = null;
+	
+	/// <summary>
+	/// Class Constructor
+	/// </summary>
+	/// <param name="driver">WebDriver Object</param>
+	public ResourcesPage(IWebDriver driver)
+	{
+		this._driver = driver
+		PageFactory.InitElements(_driver, this);
+	}
+	
 
   }
 }
